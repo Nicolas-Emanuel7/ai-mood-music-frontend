05a2d926e9e15b67116fbf3c137c8dcf252f880d
Nicolas Emanuel <niculas20003@outlook.com>
2026-02-03 15:04:15 -0300

    feat: define application routes for onboarding, home, mood, and about pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,68 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'onboarding',
+    children: [
+      {
+        path: 'step1',
+        loadComponent: () =>
+          import('./pages/onboarding/step1/step1.component').then(
+            (m) => m.Step1Component
+          ),
+      },
+      {
+        path: 'step2',
+        loadComponent: () =>
+          import('./pages/onboarding/step2/step2.component').then(
+            (m) => m.Step2Component
+          ),
+      },
+      {
+        path: 'step3',
+        loadComponent: () =>
+          import('./pages/onboarding/step3/step3.component').then(
+            (m) => m.Step3Component
+          ),
+      },
+    ],
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'mood',
+    children: [
+      {
+        path: 'text',
+        loadComponent: () =>
+          import('./pages/mood/text/text.component').then(
+            (m) => m.MoodTextComponent
+          ),
+      },
+      {
+        path: 'microphone',
+        loadComponent: () =>
+          import('./pages/mood/microphone/microphone.component').then(
+            (m) => m.MoodMicrophoneComponent
+          ),
+      },
+      {
+        path: 'facial',
+        loadComponent: () =>
+          import('./pages/mood/facial/facial.component').then(
+            (m) => m.MoodFacialComponent
+          ),
+      },
+    ],
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about/about.component').then((m) => m.AboutComponent),
+  },
+  { path: '', redirectTo: 'onboarding/step1', pathMatch: 'full' },
+  { path: '**', redirectTo: 'onboarding/step1' },
+];
