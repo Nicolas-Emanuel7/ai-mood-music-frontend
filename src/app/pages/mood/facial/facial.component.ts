19ff6454db6537e5cc944ba63d1176afb8653104
Nicolas Emanuel <niculas20003@outlook.com>
2026-02-03 15:04:44 -0300

    feat: create facial component with HTML, SCSS, and TypeScript for mood expression

---
+++ src/app/pages/mood/facial/facial.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-mood-facial',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './facial.component.html',
+  styleUrl: './facial.component.scss',
+})
+export class MoodFacialComponent {}
