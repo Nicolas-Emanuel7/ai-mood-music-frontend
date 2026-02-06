2db008a4e2659aad8e70f5676be7b09bce4274d7
Nicolas Emanuel <niculas20003@outlook.com>
2026-02-06 10:12:49 -0300

    feat: overhaul text component layout and functionality for improved user experience

--- src/app/pages/mood/text/text.component.ts
+++ src/app/pages/mood/text/text.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mood-text',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './text.component.html',
   styleUrl: './text.component.scss',
 })
-export class MoodTextComponent {}
+export class MoodTextComponent implements AfterViewInit {
+  @ViewChild('textInput') textInputRef!: ElementRef<HTMLTextAreaElement>;
+
+  text = '';
+
+  ngAfterViewInit(): void {
+    setTimeout(() => this.textInputRef?.nativeElement?.focus(), 100);
+  }
+}
